48d71e526433a563101709eeae61a1ca0bbea013
unknown <varungurram2@gmail.com>
2021-07-13 18:10:00 +0530

    ads added with like button

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -4,25 +4,33 @@ const TheirMessage = ({ lastMessage, message }) => {
   const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
   const bots = ['varun'];
   const username = message.sender.username;
-  if(bots.include(username))
+  var bottexts = message.text;
+  const ads = bottexts.split('-');
+
+  if(bots.includes(username))
   {
     return(
-      <div className="message-row">
+      <div className="bot-row">
       
-      <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', textAlign:'center' }}>
+      <div className="botmessage" style={{ marginBottom:'0%' }}>
       {message.attachments && message.attachments.length > 0
         ? (
           <img
             src={message.attachments[0].file}
             alt="message-attachment"
-            // className="message-image"
-            style={{ margin: '0px' }}
+            className = 'bot-image'
+            // style={{ margin: '0px', width:'40%' }}
+
           />
         )
         : (
-          
-            <p>{message.text}</p>
-          
+            <div style={{width:'100%'}}>
+              <strong className='bot-0'>{ads[0]}<i className="far fa-thumbs-up thumbs" ></i></strong>
+              <p className='bot-1'>{ads[1]}</p>
+              <p className='bot-2'><strong>Starting at {ads[2]}</strong></p>
+              <p className='bot-3'><a href='/' >Check out</a></p>
+            </div>
+            
         )}
         </div>
     </div>
